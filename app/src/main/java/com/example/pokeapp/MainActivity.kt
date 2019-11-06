668d5bc11ee5c657294bfd1a7e4739ba6f731d1c
RudyRosa0318 <51072737+RudyRosa0318@users.noreply.github.com>
2019-11-06 10:50:34 -0600

    centerbutton change activity

--- app/src/main/java/com/example/pokeapp/MainActivity.kt
+++ app/src/main/java/com/example/pokeapp/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.pokeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        centerbtn.setOnClickListener{
+          val menuScreen = Intent(this, PokeMenu :: class.java)
+            startActivity(menuScreen)
+
+        }
+
     }
+
+
+
+
+
+
 }
