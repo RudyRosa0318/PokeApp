477fd801561c745786675cd5963018c9df8054e8
RudyRosa0318 <51072737+RudyRosa0318@users.noreply.github.com>
2019-11-06 10:51:01 -0600

    commit de gradle

---
+++ app/src/main/java/com/example/pokeapp/PokeMenu.kt
@@ -0,0 +1,12 @@
+package com.example.pokeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PokeMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_poke_menu)
+    }
+}
