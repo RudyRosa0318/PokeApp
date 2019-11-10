d485c85125645bcb1ed0b70058fa54045f1df4e3
RudyRosa0318 <51072737+RudyRosa0318@users.noreply.github.com>
2019-11-09 19:42:23 -0600

    botones del fragment

--- app/src/main/java/com/example/pokeapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pokeapp/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.pokeapp.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.pokeapp.MenuPoke
 import com.example.pokeapp.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -18,6 +21,9 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
+
     ): View? {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
@@ -25,7 +31,12 @@ class HomeFragment : Fragment() {
         val textView: TextView = root.findViewById(R.id.text_selecciona)
         homeViewModel.text.observe(this, Observer {
             textView.text = "Seleccione su Pokemon"
+
+
         })
         return root
     }
+
+
+
 }
